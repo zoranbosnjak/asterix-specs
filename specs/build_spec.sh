0cf2939e48b5b49ccf0435b410e5aec0e14392eb
Zoran Bošnjak <zoran.bosnjak@via.si>
2021-11-24 10:07:04 +0100

    auxilary script minor update

--- specs/build_spec.sh
+++ specs/build_spec.sh
@@ -1,5 +1,26 @@
 #! /usr/bin/env bash
 
+warnings="--warnings"
+
+while getopts ":hd" opt; do
+  case ${opt} in
+    h)
+        echo "Usage:"
+        echo "    $0 -h                Display this help message."
+        echo "    $0 file.ast          Process file.ast."
+        echo "    $0 -d file.ast       Process file.ast, disable warnings check."
+        exit 0
+        ;;
+    d)
+        warnings=""
+        ;;
+    \? )
+        echo "Invalid Option: -$OPTARG" 1>&2
+        exit 1
+  esac
+done
+shift $((OPTIND -1))
+
 if [ $# -ne 1 ]; then
     echo "Expecting .ast file as argument."
     exit 1
@@ -10,7 +31,7 @@ set -e
 src=$1
 dst="output"
 
-aspecs validate -f $src --ast --warnings > /dev/null
+aspecs validate -f $src --ast $warnings > /dev/null
 cp $src $dst/definition.ast
 aspecs convert -f $src --ast --ast > $dst/definition.txt
 aspecs convert -f $src --ast --json > $dst/definition.json
