052904fe93c87722fc57cb8dac3da33e9e643f95
Zoran Bošnjak <zoran.bosnjak@via.si>
2024-04-30 12:44:45 +0200

    json syntax added

--- aspecs/lib/Asterix/Specs/Syntaxes.hs
+++ aspecs/lib/Asterix/Specs/Syntaxes.hs
@@ -8,9 +8,11 @@ module Asterix.Specs.Syntaxes
 import           Asterix.Specs.Syntax
 import qualified Asterix.Specs.Syntax.Ast      as Sast
 import qualified Asterix.Specs.Syntax.Internal as Sint
+import qualified Asterix.Specs.Syntax.Json     as Sjson
 
 syntaxes :: [(String, Coder)]
 syntaxes =
     [ ("internal", Sint.coder)
     , ("ast", Sast.coder)
+    , ("json", Sjson.coder)
     ]
